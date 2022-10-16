ad7645ed19b6e261253e782fb079d25a0c42fd1a
Farhan Khan <farhan.k2005@gmail.com>
2022-10-16 15:34:15 -0400

    ADMINS CAN VIEW AND MASS DELETE STUDENTS

---
+++ app/Orchid/Layouts/ViewStudentLayout.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Orchid\Layouts;
+
+use Orchid\Screen\TD;
+use App\Models\Student;
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Layouts\Table;
+use Orchid\Screen\Fields\CheckBox;
+
+class ViewStudentLayout extends Table
+{
+    /**
+     * Data source.
+     *
+     * The name of the key to fetch it from the query.
+     * The results of which will be elements of the table.
+     *
+     * @var string
+     */
+    protected $target = 'students';
+
+    /**
+     * Get the table cells to be displayed.
+     *
+     * @return TD[]
+     */
+    protected function columns(): iterable
+    {
+        return [
+            TD::make()
+                ->render(function (Student $student){
+                    return CheckBox::make('students[]')
+                        ->value($student->id)
+                        ->checked(false);
+                }),
+            TD::make('firstname', 'First Name')
+                ->render(function (Student $student) {
+                    return Link::make($student->firstname)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('lastname', 'Last Name')
+                ->render(function (Student $student) {
+                    return Link::make($student->lastname)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('email', 'Email')
+                ->render(function (Student $student) {
+                    return Link::make($student->email)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('school', 'School')
+                ->render(function (Student $student) {
+                    return Link::make($student->school)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('grade', 'Grade')
+                ->render(function (Student $student) {
+                    return Link::make($student->grade)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('ticketstatus', 'Ticket Status')
+                ->render(function (Student $student) {
+                    return Link::make($student->ticketstatus)
+                        ->route('platform.student.edit', $student);
+                }),
+            TD::make('allergies', 'Allergies')
+                ->render(function (Student $student) {
+                    return Link::make($student->allergies)
+                        ->route('platform.student.edit', $student);
+                }),
+        ];
+    }
+}
