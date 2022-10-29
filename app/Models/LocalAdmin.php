8b620fb0c3e0e4b1e6131783409304abd8b40266
Farhan Khan <farhan.k2005@gmail.com>
2022-10-29 17:56:27 -0400

    started filtering for localadmins

--- app/Models/LocalAdmin.php
+++ app/Models/LocalAdmin.php
@@ -12,4 +12,29 @@ class Localadmin extends Model
     use AsSource;
 
     protected $fillable = ['user_id', 'firstname', 'lastname', 'phonenumber', 'email', 'school'];
+
+    public function scopeFilter($query, array $filters){
+
+        $query  ->join('users', 'users.id', '=', 'localadmins.user_id')
+                ->join('schools', 'school_name', '=', 'school');
+
+
+        if(isset($filters['school'])){
+            $query ->where('school', 'like', '%' . request('school') . '%');
+        }
+
+        if(isset($filters['country'])){
+            $query->where('users.country', 'like', '%' . request('country') . '%');
+        }
+
+        if(isset($filters['school_board'])){
+            $query->where('school_board', 'like', '%' . request('school_board') . '%');
+        }
+
+        if(isset($filters['state_province'])){
+            $query->where('state_province', 'like', '%' . request('state_province') . '%');
+        }
+
+        $query->select('localadmins.*');
+    }
 }
