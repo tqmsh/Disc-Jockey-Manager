be80a2c9115fbc60ba8c81376ab0e09a696d70cc
Farhan Khan <farhan.k2005@gmail.com>
2022-11-20 01:04:09 -0500

    put all filtering in try and catch and made it so superadmin can filter schools by county

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Exception;
 use App\Models\School;
 use Orchid\Screen\AsSource;
+use Orchid\Support\Facades\Alert;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -17,26 +19,34 @@ class Events extends Model
 
     public function scopeFilter($query, array $filters){
 
-        $query->join('schools', 'schools.id', '=', 'school_id');
+        try{
+
+            $query->join('schools', 'schools.id', '=', 'school_id');
 
 
-        if(isset($filters['school'])){
-            $query ->where('school', 'like', '%' . request('school') . '%');
+            if(isset($filters['school'])){
+                $query ->where('school', 'like', '%' . request('school') . '%');
+            }
+
+            if(isset($filters['country'])){
+                $query->where('country', 'like', '%' . request('country') . '%');
+            }
+
+            if(isset($filters['school_board'])){
+                $query->where('school_board', 'like', '%' . request('school_board') . '%');
+            }
+
+            if(isset($filters['state_province'])){
+                $query->where('state_province', 'like', '%' . request('state_province') . '%');
+            }
+
+            $query->select('events.*');
+
+
+        }catch(Exception $e){
+
+            Alert::error('There was an error processing the filter. Error Message: ' . $e);
         }
-
-        if(isset($filters['country'])){
-            $query->where('country', 'like', '%' . request('country') . '%');
-        }
-
-        if(isset($filters['school_board'])){
-            $query->where('school_board', 'like', '%' . request('school_board') . '%');
-        }
-
-        if(isset($filters['state_province'])){
-            $query->where('state_province', 'like', '%' . request('state_province') . '%');
-        }
-
-        $query->select('events.*');
     }
 
     public function getFullAttribute(): string
