89ae4e5a8c02e35934542a74bec189897190755f
Farhan Khan <farhan.k2005@gmail.com>
2022-11-21 15:57:02 -0500

    removed the default users section

--- routes/platform.php
+++ routes/platform.php
@@ -102,33 +102,6 @@ Route::screen('profile', UserProfileScreen::class)
             ->push(__('Profile'), route('platform.profile'));
     });
 
-// Platform > System > Users
-Route::screen('users/{user}/edit', UserEditScreen::class)
-    ->name('platform.systems.users.edit')
-    ->breadcrumbs(function (Trail $trail, $user) {
-        return $trail
-            ->parent('platform.systems.users')
-            ->push(__('User'), route('platform.systems.users.edit', $user));
-    });
-
-// Platform > System > Users > Create
-Route::screen('users/create', UserEditScreen::class)
-    ->name('platform.systems.users.create')
-    ->breadcrumbs(function (Trail $trail) {
-        return $trail
-            ->parent('platform.systems.users')
-            ->push(__('Create'), route('platform.systems.users.create'));
-    });
-
-// Platform > System > Users > User
-Route::screen('users', UserListScreen::class)
-    ->name('platform.systems.users')
-    ->breadcrumbs(function (Trail $trail) {
-        return $trail
-            ->parent('platform.index')
-            ->push(__('Users'), route('platform.systems.users'));
-    });
-
 // Platform > System > Roles > Role
 Route::screen('roles/{role}/edit', RoleEditScreen::class)
     ->name('platform.systems.roles.edit')
