5f3f0687db7dc97e1ef0550c0e2ba57102951bdb
Farhan Khan <farhan.k2005@gmail.com>
2022-11-29 19:06:25 -0500

    local admins can no longer manually enter a student id that is not in their school in the url and edit them

--- local_admin/app/Models/Student.php
+++ local_admin/app/Models/Student.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Exception;
 use App\Models\User;
+use App\Models\Localadmin;
 use Orchid\Screen\AsSource;
 use Orchid\Support\Facades\Alert;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
