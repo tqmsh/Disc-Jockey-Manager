fe25513072de2fb9a8dfbc21ce6a63246f00fc02
Farhan Khan <farhan.k2005@gmail.com>
2022-12-01 20:12:41 -0500

    fixed a bug where the students wern't getting the permissions after a localadmin creates them directly

--- local_admin/app/Models/Student.php
+++ local_admin/app/Models/Student.php
@@ -16,7 +16,7 @@ class Student extends Model
     use HasFactory;
     use AsSource;
 
-    protected $fillable = ['user_id', 'school_id', 'created_at', 'updated_at', 'firstname', 'lastname', 'grade', 'phonenumber', 'email', 'ticketstatus', 'table_id', 'school', 'event_id', 'allergies'];
+    protected $fillable = ['user_id', 'school_id', 'account_status', 'created_at', 'updated_at', 'firstname', 'lastname', 'grade', 'phonenumber', 'email', 'ticketstatus', 'table_id', 'school', 'event_id', 'allergies'];
     
     public function scopeFilter($query, array $filters){
         
