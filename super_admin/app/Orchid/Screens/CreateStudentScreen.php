029d3a53b5b47a09449e5b375d3f0e3ef3336833
Farhan Khan <farhan.k2005@gmail.com>
2022-12-05 14:04:53 -0500

    fixed account status bug

--- super_admin/app/Orchid/Screens/CreateStudentScreen.php
+++ super_admin/app/Orchid/Screens/CreateStudentScreen.php
@@ -238,7 +238,7 @@ class CreateStudentScreen extends Screen
             'school' => $request->input('school'),
             'school_id' => $school_id,
             'grade' => $request->input('grade'),
-            'account_status' =>1,
+            'account_status' => 1,
             'event_id' => $request->input('event_id'),
             'allergies' => $request->input('allergies'),
             'ticketstatus'=> $request->input('ticketstatus'),
