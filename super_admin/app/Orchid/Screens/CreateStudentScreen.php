dc252b6d3bf945ef50c2cb9e4d39c71b26396b05
Farhan Khan <farhan.k2005@gmail.com>
2022-12-09 18:02:45 -0500

    started mass import for students and made code much more readable

--- super_admin/app/Orchid/Screens/CreateStudentScreen.php
+++ super_admin/app/Orchid/Screens/CreateStudentScreen.php
@@ -2,6 +2,7 @@
 
 namespace App\Orchid\Screens;
 
+use DateTime;
 use Exception;
 use App\Models\User;
 use App\Models\Events;
@@ -19,9 +20,12 @@ use Orchid\Support\Facades\Toast;
 use Orchid\Screen\Fields\Password;
 use Orchid\Screen\Fields\Relation;
 use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Actions\ModalToggle;
 
 class CreateStudentScreen extends Screen
 {
+    public $requiredFields = ['firstname', 'lastname', 'email', 'county', 'password', 'phonenumber', 'allergies', 'grade', 'school', 'state_province', 'country', 'county'];
+
     /**
      * Query data.
      *
@@ -53,6 +57,11 @@ class CreateStudentScreen extends Screen
             Button::make('Add')
                 ->icon('plus')
                 ->method('createStudent'),
+                
+            ModalToggle::make('Mass Import Students')
+                ->modal('massImportModal')
+                ->method('massImport')
+                ->icon('plus'),
 
             Link::make('Back')
                 ->icon('arrow-left')
@@ -68,7 +77,32 @@ class CreateStudentScreen extends Screen
     public function layout(): iterable
     {
         return [
-            
+
+            Layout::modal('massImportModal',[
+
+                Layout::rows([
+
+                    Input::make('student_csv')
+                        ->type('file')
+                        ->title('File must be in csv format. Ex. students.csv')
+                        ->help('The csv file MUST HAVE these fields and they need to be named accordingly to successfully import the students: <br>
+                            • firstname <br>
+                            • lastname <br>
+                            • grade <br>
+                            • phonenumber <br>
+                            • email <br>
+                            • password <br>
+                            • allergies <br>
+                            • school <br>
+                            • state_province <br>
+                            • country <br>
+                            • county <br>')
+                ]),
+            ])
+            ->title('Mass Import Schools')
+            ->applyButton('Import')
+            ->withoutCloseButton(),
+
             Layout::rows([
 
                 Input::make('firstname')
@@ -188,7 +222,7 @@ class CreateStudentScreen extends Screen
             $userTableFields = $this->getUserFields($request);
 
             //check for duplicate email
-            if($this->validEmail($request)){
+            if($this->validEmail($request->input('email'))){
                 
                 //no duplicates found
                 User::create($userTableFields);
@@ -212,23 +246,117 @@ class CreateStudentScreen extends Screen
             Alert::error('There was an error creating this student. Error Code: ' . $e->getMessage());
         }
     }
+    //this method will mass import schools from a csv file
+    public function massImport(Request $request){
+
+        try{
+
+            $path = $this->validFile($request);
+
+            if($path){
+
+                $students = $this->csvToArray($path);
+
+                $data = [];
+
+                $keys = array_keys($students[0]);
+
+                //check if the user has the required values
+                foreach($this->requiredFields as $field){
+
+                    if(!in_array($field, $keys)){
+                        Toast::error('There are missing field(s) in your csv file.'); return;
+                    }
+                }
+
+                //loop through the array of schools and re-write the keys to insert in db
+                for ($i = 0; $i < count($students); $i ++){
+
+                    if($this->validEmail($students[$i]['email'])){
+
+
+                    }
+                }
+
+
+                Toast::success('Students imported successfully!');
+
+                return redirect()->route('platform.student.list');
+            }else{
+                Toast::error('An unknown error has occured, please contact one of the admins of Prom Planner.');
+            }
+        }catch(Exception $e){
+            
+            Alert::error('There was an error mass importing the students. Error Code: ' . $e);
+        }
+    }
+
+    private function validFile(Request $request){
+
+        $path = '';
+
+        if(!is_null($request->file('school_csv'))){
+
+            $path = $request->file('school_csv')->getRealPath();
+
+            if(!is_null($path)){
+
+                $extension = $request->file('school_csv')->extension();
+
+                if($extension != 'csv'){
+
+                    Toast::error('Incorrect file type.'); return false;
+                }else{
+
+                    return $path;
+                }
+
+            } else{
+                
+                Toast::error('An error has occured.'); return;
+            }
+
+        } else{
+
+            Toast::error('Upload a csv file to import schools.'); return false;
+        }
+    }
+    
+    //this function will convert the csv file to an array
+    private function csvToArray($filename = '', $delimiter = ','){
+        
+        if (!file_exists($filename) || !is_readable($filename)){
+            Alert::error('There has been an error finding this file.');
+            return;
+        }
+
+        $header = null;
+        $data = array();
+
+        if (($handle = fopen($filename, 'r')) !== false){
+
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false){
+                if (!$header)
+                    $header = $row;
+                else
+                    $data[] = array_combine($header, $row);
+            }
+
+            fclose($handle);
+        }
+
+        return $data;
+    }
 
     //check for duplicate emails
-    private function validEmail($request){
-        return count(User::where('email', $request->input('email'))->get()) == 0;
+    private function validEmail($email){
+        return count(User::where('email', $email)->get()) == 0;
     }
 
     //this functions returns the values that need to be inserted in the localadmin table in the db
     private function getStudentFields($request){
-        $school_id = School::where('school_name', $request->input('school'))
-                            ->where('county', $request->input('county'))
-                            ->where('state_province', $request->input('state_province'))
-                            ->where('country', $request->input('country'))
-                            ->get('id')->value('id');
 
-        if(is_null($school_id)){
-            throw New Exception('You are trying to enter a invalid school');
-        }
+        $school_id = $this->getSchoolID($request);
 
         $studentTableFields = [
             'firstname' => $request->input('firstname'),
@@ -246,6 +374,23 @@ class CreateStudentScreen extends Screen
         ];
         
         return $studentTableFields;
+    }
+
+    private function getSchoolID($request){
+        $school_id = School::where('school_name', $request->input('school'))
+                            ->where('county', $request->input('county'))
+                            ->where('state_province', $request->input('state_province'))
+                            ->where('country', $request->input('country'))
+                            ->get('id')->value('id');
+
+        if(is_null($school_id)){
+
+            throw New Exception('You are trying to enter a invalid school');
+
+        } else{
+
+            return $school_id;
+        }
     }
 
     //this functions returns the values that need to be inserted in the user table in the db
