a8e67dd60934382a90487a70072746b172d73471
Farhan Khan <farhan.k2005@gmail.com>
2022-12-16 17:01:38 -0500

    Country, country, state/province & school automatically get imputed from the school's table when a local admin creates a event.

--- local_admin/app/Orchid/Screens/CreateEventScreen.php
+++ local_admin/app/Orchid/Screens/CreateEventScreen.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Models\Events;
 use App\Models\School;
 use Orchid\Screen\Screen;
+use App\Models\Localadmin;
 use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Fields\Input;
@@ -15,6 +16,7 @@ use Orchid\Support\Facades\Alert;
 use Orchid\Support\Facades\Toast;
 use Orchid\Support\Facades\Layout;
 use Orchid\Screen\Fields\DateTimer;
+use Illuminate\Support\Facades\Auth;
 
 class CreateEventScreen extends Screen
 {
@@ -75,93 +77,43 @@ class CreateEventScreen extends Screen
 
                 DateTimer::make('event_start_time')
                     ->title('Event Start')
-                    ->required()
                     ->horizontal()
                     ->allowInput()
+                    ->required()
                     ->enableTime(),
 
                 DateTimer::make('event_finish_time')
                     ->title('Event End')
-                    ->required()
                     ->horizontal()
                     ->allowInput()
+                    ->required()
                     ->enableTime(),
-                    
-                Input::make('event_address')
-                    ->title('Event Address')
-                    ->type('text')
-                    ->required()
-                    ->placeholder('Ex. 2381 Ogilvie Rd')
-                    ->horizontal(),
-                    
-                Input::make('event_zip_postal')
-                    ->title('Event Zip/Postal')
-                    ->type('text')
-                    ->required()
-                    ->placeholder('Ex. K1J 7N4')
-                    ->horizontal(),
 
                 Input::make('event_info')
                     ->title('Event Info')
                     ->type('text')
-                    ->required()
                     ->placeholder('Ex. Formal Attire')
                     ->horizontal(),
 
                 Input::make('event_rules')
                     ->title('Event Rules')
                     ->type('text')
-                    ->required()
                     ->placeholder('Ex. No Violence')
                     ->horizontal(),
-
-                Select::make('school')
-                    ->title('School')
-                    ->required()
-                    ->horizontal()
-                    ->empty('No Selection')
-                    ->fromModel(School::class, 'school_name', 'school_name'),
-
-                Select::make('country')
-                    ->title('Country')
-                    ->required()
-                    ->empty('No Selection')
-                    ->horizontal()
-                    ->fromModel(School::class, 'country', 'country'),
-
-                Select::make('county')
-                    ->title('County')
-                    ->required()
-                    ->empty('No Selection')
-                    ->horizontal()
-                    ->fromModel(School::class, 'county', 'county'),
-
-                Select::make('state_province')
-                    ->title('State/Province')
-                    ->empty('No Selection')
-                    ->horizontal()
-                    ->fromModel(School::class, 'state_province', 'state_province'),
             ]),
         ];
     }
 
     public function createEvent(Request $request){
 
+        $school = School::where('id', Localadmin::where('user_id', Auth::user()->id)->get('school_id')->value('school_id'))->first();
+
         try{
-
-            $school_id = School::where('school_name', $request->input('school'))
-                                ->where('county', $request->input('county'))
-                                ->where('state_province', $request->input('state_province'))
-                                ->where('country', $request->input('country'))
-                                ->get('id')->value('id');
-
-            if(is_null($school_id)){
-                throw New Exception('You are trying to enter a invalid school');
-            }
 
             $formFields = $request->all();
             $formFields['event_creator'] = auth()->id();
-            $formFields['school_id'] = $school_id;
+            $formFields['school_id'] = $school->id;
+            $formFields['school'] = $school->school_name;
 
             Events::create($formFields);
 
