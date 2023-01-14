e92a0e95c9fdf3b8e85cf578ef3c25ee376959ca
Farhan Khan <farhan.k2005@gmail.com>
2023-01-14 14:28:48 -0500

    added the sanctum security for protecting api routes

--- super_admin/app/Models/User.php
+++ super_admin/app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Orchid\Platform\Models\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+    
     /**
      * The attributes that are mass assignable.
      *
