c13fe1785f9e6c99e19e28caa83e6b04bd77e6d8
Farhan Khan <farhan.k2005@gmail.com>
2023-01-18 14:02:29 -0500

    added the models for the api

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,43 +2,87 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Orchid\Platform\Models\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    use HasApiTokens;
+    
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
+        'id',
+        'firstname',
+        'lastname',
+        'country',
+        'role',
+        'phonenumber',
         'name',
         'email',
+        'account_status',
         'password',
+        'updated_at',
+        'created_at',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes excluded from the model's JSON form.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'permissions',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'permissions'          => 'array',
+        'email_verified_at'    => 'datetime',
+    ];
+
+    /**
+     * The attributes for which you can use filters in url.
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'id',
+        'firstname',
+        'lastname',
+        'country',
+        'role',
+        'phonenumber',
+        'name',
+        'email',
+        'updated_at',
+        'created_at',
+    ];
+
+    /**
+     * The attributes for which can use sort in url.
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'id',
+        'firstname',
+        'lastname',
+        'country',
+        'role',
+        'phonenumber',
+        'name',
+        'email',
+        'updated_at',
+        'created_at',
     ];
 }
