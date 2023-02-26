9f4d7699cd2514e7bc6f331dcf8d920a10e5ddc6
goodyhi <andyt20042011@gmail.com>
2023-02-25 20:03:17 -0500

    Updated register location for readability

--- api/app/Http/Controllers/StudentController.php
+++ api/app/Http/Controllers/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Student;
+use App\Models\School;
 use App\Models\Localadmin;
 use Illuminate\Http\Request;
 
@@ -103,4 +105,57 @@ class StudentController extends Controller
     public function destroy($id)
     {
         //
-    }}
+    }
+
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed', 
+            'role' => 'required|numeric',
+            'phonenumber' => 'required|string',
+            'school_name' => 'required|string',
+            'country' => 'required|string',
+            'state_province' => 'required|string',
+            'county' => 'required|string',
+        ]);
+
+        $match = ['school_name' => $fields['school_name'], 'country' => $fields['country'], 'state_province' => $fields['state_province'], 'county' => $fields['county']];
+
+         //Find the school in where the school_name, country, state_rpovince and county matches
+        $school=School::where($match)->first();
+        if(!$school){
+            return response()->json(['message' => 'School not found'], 404);
+        }else{
+            $fields['school_id'] = $school->id;
+        }
+        
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'role' => $fields['role'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $student = Student::create([
+            'name' => $fields['name'],
+            'user_id' => $user->id,
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+            'role' => $fields['role'],
+            'school_id' => $fields['school_id'],
+            'school' => $fields['school_name'],
+            'phonenumber' => $fields['phonenumber']
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
+    
