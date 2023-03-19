186b0d4372b4aa1fc0571281e554ef2bdd7fedec
Stanley Wang <97454539+swang142@users.noreply.github.com>
2023-03-18 23:14:17 -0400

    Add files via upload

--- local_admin/app/Orchid/Layouts/ViewEventLayout.php
+++ local_admin/app/Orchid/Layouts/ViewEventLayout.php
@@ -85,6 +85,12 @@ class ViewEventLayout extends Table
                     return Link::make($event->event_rules)
                         ->route('platform.event.edit', $event);
                 }),
+            TD::make()
+                ->render(function (Events $event) {
+                    return Button::make('Edit')-> type(Color::PRIMARY())->  method('redirect', ['event'=>$event->id, 'type'=>"edit"]) ->icon('pencil');
+                }),
+
+
         ];    
     }
 }
