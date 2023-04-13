36414eb4ec21153e9db47b51f016a7f0b309aa94
Stanley Wang <97454539+swang142@users.noreply.github.com>
2023-04-13 00:11:48 -0400

    super_admin edits

---
+++ super_admin/app/Models/Song.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'artist'
+    ];
+
+    public function getFullAttribute(): string{
+        return $this->attributes['title'] . ' - d' . $this->attributes['artist'];
+    }
+}
