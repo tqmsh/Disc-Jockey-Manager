7ebd562f0dc0e5f3e845cafc53c828773ac1936b
Vlad <vlad.wpg@gmail.com>
2023-04-23 19:49:46 -0400

    Added API routes

---
+++ local_admin/app/Http/Controllers/AdController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Campaign;
+
+class AdController extends Controller
+{
+    public function impression($id){
+        Campaign::find($id)->update([
+                "impressions" => Campaign::find($id)->impressions + 1]
+        );
+    }
+    public function click($id){
+        Campaign::find($id)->update([
+                "clicks" => Campaign::find($id)->clicks + 1]
+        );
+    }
+}
