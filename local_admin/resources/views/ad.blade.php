afe96e6a411df0e2438bc63adc963c7a1cd890e1
Vlad <vlad.wpg@gmail.com>
2023-04-25 17:29:38 -0400

    Fixed Ad Display mechanism to work for multiple ads

---
+++ local_admin/resources/views/ad.blade.php
@@ -0,0 +1,54 @@
+<div>
+    <script>
+        var triggered = false;
+
+        function sendInternalRequestWithIdParam(id) {
+            if (!triggered) {
+                // prepare the request URL with ID parameter
+                var url = '/campaign_view/' + encodeURIComponent(id);
+
+                $.post(url, "", (data, status) => {
+                    console.log(data);
+                })
+                triggered = true;
+            }
+
+        }
+
+        function checkIfImageIsVisible(image, callback) {
+            if (image != null) {
+                var rect = image.getBoundingClientRect();
+                var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+                if (rect.bottom >= 0 && rect.top < viewHeight) {
+                    callback(image);
+                }
+            }
+        }
+
+        function handleVisibleImage(image) {
+            if (!triggered) {
+                console.log('Image is now visible:', image.id);
+                sendInternalRequestWithIdParam({{  $id  }})
+            }
+        }
+
+        window.addEventListener('scroll', function () {
+            checkIfImageIsVisible(image, handleVisibleImage);
+        });
+        window.addEventListener("load", function () {
+            image = document.getElementById({{  $id  }})
+            console.log(image == null);
+            checkIfImageIsVisible(image, handleVisibleImage);
+        })
+        document.getElementById({{  $id  }}).addEventListener("click", function () {
+            var url = '/campaign_click/' + encodeURIComponent(id);
+
+            $.post(url, "", (data, status) => {
+                console.log(data);
+            })
+        })
+    </script>
+    <a href="{{ $forward_url }}">
+        <img id={{  $id  }} src="{{  $image_url  }}" alt="AnImage" width="600" height="600">
+    </a>
+</div>
