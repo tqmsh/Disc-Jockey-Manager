60069ed896529bf8ecabb9d205914521663b9efc
Vlad <vlad.wpg@gmail.com>
2023-05-12 10:42:29 -0400

    Fixed regions not showing up

--- super_admin/app/Orchid/Screens/EditAdScreen.php
+++ super_admin/app/Orchid/Screens/EditAdScreen.php
@@ -5,6 +5,7 @@ namespace App\Orchid\Screens;
 use App\Models\Campaign;
 use App\Models\Categories;
 use App\Models\Region;
+use App\Models\User;
 use App\Models\Vendors;
 use Exception;
 use Illuminate\Http\Request;
@@ -32,7 +33,8 @@ class EditAdScreen extends Screen
     public function query(Campaign $campaign): iterable
     {
         $this->vendor = Vendors::where('user_id', $campaign->user_id)->first();
-        $array = Auth::user()->paidRegions->toArray();
+        $this->vendor_user = User::where("id", $this->vendor->user_id)->first();
+        $array = $this->vendor_user->paidRegions->toArray();
 
         //get all the region_ids of the array
         $this->paidRegionIds =  Arr::pluck($array, ['region_id']);
@@ -63,6 +65,7 @@ class EditAdScreen extends Screen
             Button::make('Update')
                 ->icon('check')
                 ->method('update'),
+
             Button::make('Delete Campaign')
                 ->icon('trash')
                 ->method('delete'),
