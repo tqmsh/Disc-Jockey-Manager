6300e498ffde3176a0954a3060c2dfff6040d434
Vlad <vlad.wpg@gmail.com>
2023-06-01 09:45:08 -0400

    Ad support files added to superadmin and vendor portals

---
+++ super_admin/resources/views/card_style.blade.php
@@ -0,0 +1,87 @@
+<style>
+    .card {
+        --bg: #e2e2e3;
+        --hover-bg: #8b21d7;
+        --hover-text: #ffffff;
+        text-align: center;
+        background: var(--bg);
+        padding-block: 1.3em;
+        border-radius: 3px;
+        position: relative;
+        overflow: hidden;
+        transition: .3s cubic-bezier(.6,.4,0,1),transform .15s ease;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1em;
+        padding: 1.2em 1.2em 0.3em;
+        max-width: calc(600px + 2.4em);
+    }
+
+    .card__body {
+        color: #3d3d3d;
+        line-height: 1.5em;
+        font-size: 1.5em;
+    }
+
+    .card > :not(span) {
+        transition: .3s cubic-bezier(.6,.4,0,1);
+    }
+
+    .card > strong {
+        vertical-align: center;
+        display: block;
+        font-size: 1.4rem;
+        letter-spacing: -.035em;
+    }
+
+    .card span {
+        font-size: 20em;
+        position: absolute;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: var(--hover-text);
+        border-radius: 3px;
+        font-weight: bold;
+        top: 10000%;
+        transition: all .3s cubic-bezier(.6,.4,0,1);
+    }
+
+    .card:hover span {
+        top: 0;
+        font-size: 1.4em;
+    }
+
+    .card:hover {
+        background: var(--hover-bg);
+    }
+
+    .card:hover>div,.card:hover>strong {
+        opacity: 0;
+    }
+    @media screen and (max-width: 991px){
+        img{
+            width: min(calc(100% - 20px), 300px);
+            height: min(calc(100% - 20px), 300px);
+        }
+    }
+    @media screen and (max-width: 767px){
+        img{
+            width: min(calc(100% - 20px), 300px);
+            height: min(calc(100% - 20px), 300px);
+        }
+    }
+    @media screen and (max-width: 479px){
+        img{
+            width: min(calc(100% - 20px), 300px);
+            height: min(calc(100% - 20px), 300px);
+        }
+    }
+
+
+</style>
