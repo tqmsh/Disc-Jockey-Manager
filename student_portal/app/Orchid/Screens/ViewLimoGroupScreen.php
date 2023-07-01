0bbbd7d62911fb9393afb98c9527a707f853cc72
Farhan Khan <farhan.k2005@gmail.com>
2023-07-01 19:06:36 -0400

    students can see limo group info

--- student_portal/app/Orchid/Screens/ViewLimoGroupScreen.php
+++ student_portal/app/Orchid/Screens/ViewLimoGroupScreen.php
@@ -59,16 +59,24 @@ class ViewLimoGroupScreen extends Screen
         return [
           Layout::tabs([
                 'Limo Group Info' => [
-                    // Layout::legend('owned_limo_group', [
-                    //     Sight::make('name', 'Name'),
-                    //     Sight::make('capacity', 'Capacity'),
-                    //     Sight::make('date', 'Date'),
-                    //     Sight::make('pickup_location', 'Pickup Location'),
-                    //     Sight::make('dropoff_location', 'Dropoff Location'),
-                    //     Sight::make('depart_time', 'Depart Time'),
-                    //     Sight::make('dropoff_time', 'Dropoff Time'),
-                    //     Sight::make('notes', 'Notes'),
-                    // ]),
+                    Layout::legend('owned_limo_group', [
+                        Sight::make('creator_user_id', 'Owner')->render(function (LimoGroup $limoGroup) {
+
+                            if($limoGroup->owner->user_id == Auth::user()->id){
+                                return 'You';
+                            }else{
+                                return $limoGroup->owner->firstname . ' ' . $limoGroup->owner->lastname;
+                            }
+                        }),
+                        Sight::make('name', 'Name'),
+                        Sight::make('capacity', 'Capacity'),
+                        Sight::make('date', 'Date'),
+                        Sight::make('pickup_location', 'Pickup Location'),
+                        Sight::make('dropoff_location', 'Dropoff Location'),
+                        Sight::make('depart_time', 'Depart Time'),
+                        Sight::make('dropoff_time', 'Dropoff Time'),
+                        Sight::make('notes', 'Notes'),
+                    ]),
     
                 ],
                 'Members in Limo Group' => [
