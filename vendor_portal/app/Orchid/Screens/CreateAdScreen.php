79f577c390a2c5528596ed2b1c50d62dce48f6b1
Farhan Khan <farhan.k2005@gmail.com>
2023-07-24 18:01:57 -0400

    updating security

--- vendor_portal/app/Orchid/Screens/CreateAdScreen.php
+++ vendor_portal/app/Orchid/Screens/CreateAdScreen.php
@@ -113,7 +113,7 @@ class CreateAdScreen extends Screen
                     ->height(env("AD_SIZE"))
                     ->required()
                     ->help("Image to display")
-                    ->acceptedFiles('image/*, .png, .jpg, .jpeg, .gif, .svg')
+                    ->acceptedFiles('.png, .jpg, .jpeg,')
                     ->horizontal()
                 ]),
         ];
