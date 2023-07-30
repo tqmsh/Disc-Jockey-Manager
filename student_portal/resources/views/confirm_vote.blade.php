1f2cb096890948e70cef201dc31d2ff25902a932
Chloe Houvardas <chloehouvardas404@gmail.com>
2023-07-30 16:34:39 -0400

    Changed confirm vote styling

--- student_portal/resources/views/confirm_vote.blade.php
+++ student_portal/resources/views/confirm_vote.blade.php
@@ -1,17 +1,20 @@
-<div class="modal-header">
-    <h4 class="modal-title text-black fw-light">
-        Vote Change?
-    </h4>
-    <button type="button" class="btn-close" title="Close" data-bs-dismiss="modal" aria-label="Close">
-    </button>
-</div>
 
-<div class="modal-body">
-    <div class="p-4" data-confirm-target="message">You have already voted for this position. Change vote to candidate &nbsp <strong>{{ $name }}</strong> ?</div>
-</div>
+<div class="modal-content" style="background-color:#FFF3CD">
+    <div class="modal-header">
+        <h4 class="modal-title text-black fw-light">
+            <strong>
+                Vote Change?
+            </strong>
+        </h4>
+    </div>
 
-{{-- TODO STYLE THIS BUTTON AND MAKE IT DELETE OLD VOTE AND ADD NEW ONE --}}
-<button type="button" class="btn btn-link" data-bs-dismiss="modal" onclick="voting">
-    Confirm Change
-</button>
-{{-- Test --}}
+
+    <div class="modal-body">
+        <h6 data-confirm-target="message">You have already voted for this position. Change vote to candidate&nbsp <strong>{{ $name }}</strong> ?</h6>
+    </div>
+
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary round" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-warning round">Change Vote</button>
+    </div>
+</div>
