ba0a98207cd3e480dca33cc7166cd02c53eaf4fc
Chloe Houvardas <chloehouvardas404@gmail.com>
2023-07-30 17:41:34 -0400

    Changing alert to modal

--- student_portal/resources/views/confirm_vote.blade.php
+++ student_portal/resources/views/confirm_vote.blade.php
@@ -1,20 +1,21 @@
+<div class="modal" tabindex="-1">
+    <div class="modal-content" style="background-color:#FFF3CD">
+        <div class="modal-header">
+            <h4 class="modal-title text-black fw-light">
+                <strong>
+                    Vote Change?
+                </strong>
+            </h4>
+        </div>
 
-<div class="modal-content" style="background-color:#FFF3CD">
-    <div class="modal-header">
-        <h4 class="modal-title text-black fw-light">
-            <strong>
-                Vote Change?
-            </strong>
-        </h4>
-    </div>
 
+        <div class="modal-body">
+            <h6 data-confirm-target="message">You have already voted for this position. Change vote to candidate&nbsp <strong>{{ $name }}</strong> ?</h6>
+        </div>
 
-    <div class="modal-body">
-        <h6 data-confirm-target="message">You have already voted for this position. Change vote to candidate&nbsp <strong>{{ $name }}</strong> ?</h6>
-    </div>
-
-    <div class="modal-footer">
-        <button type="button" class="btn btn-secondary round" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-warning round">Change Vote</button>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-warning">Change Vote</button>
+        </div>
     </div>
 </div>
