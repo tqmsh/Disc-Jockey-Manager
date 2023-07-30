b711199bbfa968dc9f53afef938fd40702c721fd
Chloe Houvardas <chloehouvardas404@gmail.com>
2023-07-30 15:53:20 -0400

    Added to confirm vote notif

--- student_portal/resources/views/confirm_vote.blade.php
+++ student_portal/resources/views/confirm_vote.blade.php
@@ -1,1 +1,17 @@
-You have already voted for this position. Change vote to candidate &nbsp<strong>{{ $name }}</strong> ?
+<div class="modal-header">
+    <h4 class="modal-title text-black fw-light">
+        Vote Change?
+    </h4>
+    <button type="button" class="btn-close" title="Close" data-bs-dismiss="modal" aria-label="Close">
+    </button>
+</div>
+
+<div class="modal-body">
+    <div class="p-4" data-confirm-target="message">You have already voted for this position. Change vote to candidate &nbsp <strong>{{ $name }}</strong> ?</div>
+</div>
+
+{{-- TODO STYLE THIS BUTTON AND MAKE IT DELETE OLD VOTE AND ADD NEW ONE --}}
+<button type="button" class="btn btn-link" data-bs-dismiss="modal" onclick="voting">
+    Confirm Change
+</button>
+{{-- Test --}}
