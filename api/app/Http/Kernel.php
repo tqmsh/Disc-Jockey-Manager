66d51c873da2a1c321e1eac00cc3747ea7d562fa
Farhan Khan <farhan.k2005@gmail.com>
2023-08-15 14:10:07 -0400

    cors #3

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\OwnCors::class,
+        // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
--- local_admin/app/Http/Middleware/OwnCors.php
+++ local_admin/app/Http/Middleware/OwnCors.php
@@ -14,22 +14,9 @@ class OwnCors
      * @param \Closure $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            return response('OK')
-                ->withHeaders($headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
     }
 }
