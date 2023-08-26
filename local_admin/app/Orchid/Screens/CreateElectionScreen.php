b07f9bc56ea482b19dd2787455969e18de12c97d
Chloe Houvardas <chloehouvardas404@gmail.com>
2023-08-26 19:32:03 -0400

    Got rid of unnecessary code

--- local_admin/app/Orchid/Screens/CreateElectionScreen.php
+++ local_admin/app/Orchid/Screens/CreateElectionScreen.php
@@ -98,8 +98,6 @@ class CreateElectionScreen extends Screen
         try{
 
             $electionField = $request->all();
-            $electionField['event_id'] = $event->id;
-            $electionField['school_id'] = $event->school_id;
 
             if ($electionField['end_date'] < $electionField['start_date'])
             {
