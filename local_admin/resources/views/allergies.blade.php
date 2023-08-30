fd70646410b53d27bfb7579b6008d32ccd051d05
Farhan Khan <farhan.k2005@gmail.com>
2023-08-30 14:04:12 -0400

    small edits

--- local_admin/resources/views/allergies.blade.php
+++ local_admin/resources/views/allergies.blade.php
@@ -1,7 +1,7 @@
 <div class="rounded bg-white mb-3 p-3">
     <div c5ass="flex align-items-center w-100 rounded overflow-hidden" style="min-height: 200px;">
-        <h6 class="text-muted center mb-3 font-semibold">Allergies Count</h6>
-        <ul class="center">
+        <strong><h7 class="text-muted center">Allergies Count:</h7></strong>
+        <ul class="center mt-3">
             @foreach ($allergies as $name => $count)
                 <li class="text-muted">{{$name}}: {{$count}}</li>
             @endforeach
