66746cc9544ddbda7539bfd190df25f43007ce43
Skyler Ma <skyler@skylerma.com>
2024-03-07 13:17:34 -0500

    Add table for expenses and table for revenues to viewing screen in super_admin. Add link to create screen and button for deleting selected.

--- super_admin/app/Orchid/Screens/ViewUniversalExpenseRevenueScreen.php
+++ super_admin/app/Orchid/Screens/ViewUniversalExpenseRevenueScreen.php
@@ -2,10 +2,20 @@
 
 namespace App\Orchid\Screens;
 
+use App\Models\UniversalExpenseRevenue;
+use Illuminate\Http\Request;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
+use Orchid\Screen\TD;
+use Orchid\Screen\Fields\CheckBox;
+use Orchid\Support\Color;
+use Orchid\Support\Facades\Layout;
+use Orchid\Support\Facades\Toast;
 
 class ViewUniversalExpenseRevenueScreen extends Screen
 {
+    public $expenses, $revenues;
     /**
      * Query data.
      *
@@ -13,7 +23,10 @@ class ViewUniversalExpenseRevenueScreen extends Screen
      */
     public function query(): iterable
     {
-        return [];
+        return [
+            'expenses' => UniversalExpenseRevenue::where('type', 1)->get(),
+            'revenues' => UniversalExpenseRevenue::where('type', 2)->get(),
+        ];
     }
 
     /**
@@ -23,7 +36,7 @@ class ViewUniversalExpenseRevenueScreen extends Screen
      */
     public function name(): ?string
     {
-        return 'ViewUniversalExpensesRevenuesScreen';
+        return 'Universal Expenses and Revenues';
     }
 
     /**
@@ -33,7 +46,15 @@ class ViewUniversalExpenseRevenueScreen extends Screen
      */
     public function commandBar(): iterable
     {
-        return [];
+        return [
+            Link::make('Add New Expenses and Revenues')
+                ->icon('plus')
+                ->route('platform.universal-expense-revenue.create'),
+            Button::make('Delete Selected Expenses and Revenues')
+                ->icon('trash')
+                ->method('deleteExpensesRevenues')
+                ->confirm(__('Are you sure you want to delete the selected expenses and revenues?')),
+        ];
     }
 
     /**
@@ -43,6 +64,80 @@ class ViewUniversalExpenseRevenueScreen extends Screen
      */
     public function layout(): iterable
     {
-        return [];
+        return [
+            Layout::table('expenses', [
+                TD::make()
+                    ->render(function (UniversalExpenseRevenue $expense){
+                        return CheckBox::make('selectedExpensesRevenues[]')
+                            ->value($expense->id)
+                            ->checked(false);
+                    }),
+
+                TD::make('name', 'Name')
+                    ->render(function (UniversalExpenseRevenue $expense) {
+                        return Link::make($expense->name)
+                            ->route('platform.universal-expense-revenue.edit', $expense);
+                    }),
+                TD::make('created_at', 'Created At')
+                    ->render(function (UniversalExpenseRevenue $expense) {
+                        return Link::make($expense->created_at->toDateTimeString())
+                            ->route('platform.universal-expense-revenue.edit', $expense);
+                    }),
+                TD::make('updated_at', 'Updated At')
+                    ->render(function (UniversalExpenseRevenue $expense) {
+                        return Link::make($expense->updated_at->toDateTimeString())
+                            ->route('platform.universal-expense-revenue.edit', $expense);
+                    }),
+                TD::make()
+                    ->render(function (UniversalExpenseRevenue $expense) {
+                        return Link::make('Edit')
+                            ->type(Color::PRIMARY())
+                            ->route('platform.universal-expense-revenue.edit', $expense)
+                            ->icon('pencil');
+                    }),
+            ])->title('Expenses'),
+
+            Layout::table('revenues', [
+                TD::make()
+                    ->render(function (UniversalExpenseRevenue $revenue){
+                        return CheckBox::make('selectedExpensesRevenues[]')
+                            ->value($revenue->id)
+                            ->checked(false);
+                    }),
+
+                TD::make('name', 'Name')
+                    ->render(function (UniversalExpenseRevenue $revenue) {
+                        return Link::make($revenue->name)
+                            ->route('platform.universal-expense-revenue.edit', $revenue);
+                    }),
+                TD::make('created_at', 'Created At')
+                    ->render(function (UniversalExpenseRevenue $revenue) {
+                        return Link::make($revenue->created_at->toDateTimeString())
+                            ->route('platform.universal-expense-revenue.edit', $revenue);
+                    }),
+                TD::make('updated_at', 'Updated At')
+                    ->render(function (UniversalExpenseRevenue $revenue) {
+                        return Link::make($revenue->updated_at->toDateTimeString())
+                            ->route('platform.universal-expense-revenue.edit', $revenue);
+                    }),
+                TD::make()
+                    ->render(function (UniversalExpenseRevenue $revenue) {
+                        return Link::make('Edit')
+                            ->type(Color::PRIMARY())
+                            ->route('platform.universal-expense-revenue.edit', $revenue)
+                            ->icon('pencil');
+                    }),
+            ])->title('Revenues'),
+        ];
+    }
+
+    public function deleteExpensesRevenues(Request $request) {
+        $expensesRevenues = $request->get('selectedExpensesRevenues');
+        if (!empty($expensesRevenues)) {
+            UniversalExpenseRevenue::whereIn('id', $expensesRevenues)->delete();
+            Toast::success('Selected expenses and revenues deleted succesfully');
+        } else {
+            Toast::warning('You must select expenses and/or revenues in order to delete them');
+        }
     }
 }
