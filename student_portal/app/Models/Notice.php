86284e4d6618814b938c229029ef684f9b2e873d
sky46 <skyler@skylerma.com>
2024-03-22 16:07:14 -0400

    Add notice feature to student_portal and vendor_portal. Add Notice model. View::share() notice in vendor OrchidServiceProvider. Add notice in vendor dashboard.blade.php.

---
+++ student_portal/app/Models/Notice.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
+
+class Notice extends Model
+{
+    use HasFactory, AsSource;
+    protected $fillable = [
+        'dashboard',
+        'content',
+        'url',
+    ];
+    public static $dashboard_names = [
+        2 => 'Local Admin',
+        3 => 'Student Portal',
+        4 => 'Vendor Portal',
+    ];
+    public function getDashboardName()
+    {
+        return self::$dashboard_names[$this->dashboard];
+    }
+}
