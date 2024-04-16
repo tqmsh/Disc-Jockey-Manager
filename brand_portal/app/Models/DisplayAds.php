4928a45e3106b078730d380ad8f5b431e98a8620
NotYoel <nopenotyoel@gmail.com>
2024-04-15 22:56:10 -0400

    add display ads to brand portal

---
+++ brand_portal/app/Models/DisplayAds.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Orchid\Screen\AsSource;
+
+class DisplayAds extends Model
+{
+    use HasFactory, AsSource;
+
+    protected $fillable = [
+        'route_name',
+        'ad_index',
+        'campaign_id',
+        'portal',
+        'square'
+    ];
+
+    public function campaign() : BelongsTo {
+        return $this->belongsTo(Campaign::class, 'campaign_id');
+    }
+
+    public function portalToName() : string {
+        return match($this->portal) {
+            0 => 'Local Admin',
+            1 => 'Student',
+            2 => 'Vendor'
+        };
+    }
+}
--- brand_portal/vendor/orchid/platform/resources/views/dashboard.blade.php
+++ brand_portal/vendor/orchid/platform/resources/views/dashboard.blade.php
@@ -73,6 +73,36 @@
 
     <div class="mt-3 mt-md-4">
 
+        @php
+            $display_ads_query = \App\Models\DisplayAds::where('portal', 2)
+                                    ->where('route_name', request()->route()->getName())
+                                    ->where('ad_index', 0);
+        @endphp
+        @if($display_ads_query->exists())
+            @php
+                $campaign = $display_ads_query->first()->campaign;
+                $campaign->increment('impressions');
+                $campaign->save();
+
+                // styles
+                $display_ad_square = boolval($display_ads_query->first()->square);
+                $display_ad_image_url = $campaign->image;
+                $display_ad_min_width = $display_ad_square ? "" : "min-width:40%;";
+                $display_ad_margin = $display_ad_square ? "" : "margin: 0 auto;";
+                $display_ad_max_width = $display_ad_square ? "" : "max-width:485px; max-height:60px;";
+                $display_ad_width = $display_ad_square ? "width: 311.51px; height: 311.51px;" : "width: 100%; height:auto;";
+                $display_ad_display = $display_ad_square ? "display: flex; justify-content: flex-end; width:100%;" : "";
+            @endphp
+
+            @if($display_ad_image_url !== null)
+                <div style="{{$display_ad_min_width}} {{$display_ad_margin}} {{$display_ad_max_width}} margin-bottom: 0.75rem; {{$display_ad_display}}">
+                    <a target="_blank" href="{{$campaign->website}}">
+                        <img style="{{$display_ad_width}} max-height:{{boolval($display_ads_query->first()->square) ? 311.51 : 90}}px;" src="{{$display_ad_image_url}}" alt="">
+                    </a>
+                </div>
+            @endif
+        @endif
+
         @if(Breadcrumbs::has())
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb px-4 mb-2">
