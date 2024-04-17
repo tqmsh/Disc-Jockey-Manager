190c7f445dafa989efd8590037cc9f38d9479800
Skyler Ma <skyler@skylerma.com>
2024-04-17 14:00:02 -0400

    Add submit, delete, and back buttons to commandbar in student_portal EditPromfluencerScreen.

--- student_portal/app/Orchid/Screens/EditPromfluencerScreen.php
+++ student_portal/app/Orchid/Screens/EditPromfluencerScreen.php
@@ -4,6 +4,8 @@ namespace App\Orchid\Screens;
 
 use App\Models\Promfluencer;
 use Illuminate\Support\Facades\Auth;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
 
 class EditPromfluencerScreen extends Screen
@@ -38,7 +40,18 @@ class EditPromfluencerScreen extends Screen
      */
     public function commandBar(): iterable
     {
-        return [];
+        return [
+            Button::make('Submit')
+                ->icon('check')
+                ->method('updatePromfluencer'),
+            Button::make('Delete Promfluence')
+                ->icon('trash')
+                ->method('deletePromfluencer')
+                ->confirm('Are you sure you would like to delete your Promfluence?'),
+            Link::make('Back')
+                ->icon('arrow-left')
+                ->route('platform.promfluencer.view'),
+        ];
     }
 
     /**
