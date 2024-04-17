074fe1535275ffad778b05bacdde867a6fcc0fcc
Skyler Ma <skyler@skylerma.com>
2024-04-17 13:42:30 -0400

    Add legend which renders promfluencer info  if it exists in student_portal ViewPromfluencerScreen.

--- student_portal/app/Orchid/Screens/ViewPromfluencerScreen.php
+++ student_portal/app/Orchid/Screens/ViewPromfluencerScreen.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 use Orchid\Screen\Actions\Button;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
+use Orchid\Screen\Sight;
+use Orchid\Support\Facades\Layout;
 
 class ViewPromfluencerScreen extends Screen
 {
@@ -74,6 +76,29 @@ class ViewPromfluencerScreen extends Screen
      */
     public function layout(): iterable
     {
-        return [];
+        return $this->promfluencer !== null ? [
+            Layout::legend($this->promfluencer, [
+                Sight::make('name', 'Name')
+                    ->render(function () {
+                        return ($this->promfluencer->user->firstname . ' ' . $this->promfluencer->user->lastname) ?? '';
+                    }),
+                Sight::make('email', 'Email')
+                    ->render(fn () => $this->promfluencer->user->email ?? ''),
+                Sight::make('phonenumber', 'Phone Number')
+                    ->render(fn () => $this->promfluencer->user->phonenumber ?? ''),
+                Sight::make('school', 'School')
+                    ->render(fn () => $this->promfluencer->user->student->school ?? ''),
+                Sight::make('grade', 'Grade')
+                    ->render(fn () => $this->promfluencer->user->student->grade ?? ''),
+                Sight::make('instagram', 'Instagram')
+                    ->render(fn () => $this->promfluencer->instagram ?? ''),
+                Sight::make('tiktok', 'TikTok')
+                    ->render(fn () => $this->promfluencer->tiktok ?? ''),
+                Sight::make('snapchat', 'Snapchat')
+                    ->render(fn () => $this->promfluencer->snapchat ?? ''),
+                Sight::make('YouTube', 'YouTube')
+                    ->render(fn () => $this->promfluencer->youtube ?? ''),
+            ]),
+        ] : [];
     }
 }
